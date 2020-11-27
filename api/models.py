9f06e1c8eaae9bfc459fed7e74f6c9c4f957f8b4
Ilia <kul.2002.il@gmail.com>
2020-11-27 11:54:03 +0700

    Добавлены модели.

--- api/models.py
+++ api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Author(models.Model):
 	first_name = models.CharField(max_length=255, verbose_name="Имя", default='')
 	last_name = models.CharField(max_length=255, verbose_name="Фамилия", default='')
@@ -8,10 +9,35 @@ class Author(models.Model):
 	def __str__(self):
 		return self.last_name + " " + self.first_name + " " + self.patro_name
 
+
+class Genre(models.Model):
+	title = models.CharField(max_length=255, verbose_name="Жанр", default='')
+
+	def __str__(self):
+		return self.title
+
+
+class Category(models.Model):
+	title = models.CharField(max_length=255, verbose_name="Категория", default='')
+
+	def __str__(self):
+		return self.title
+
+
+class Publisher(models.Model):
+	title = models.CharField(max_length=255, verbose_name="Издательство", default='')
+
+	def __str__(self):
+		return self.title
+
+
 class Book(models.Model):
 	title = models.CharField(max_length=255, verbose_name="Название", default='')
-	datetime = models.DateTimeField(auto_now_add=True, null=True, verbose_name='Дата публикации')
+	date = models.DateField(auto_now_add=True, null=True, verbose_name='Дата публикации')
 	author = models.ManyToManyField(Author)
+	category = models.ForeignKey(Category, on_delete=models.DO_NOTHING())
+	genre = models.ManyToManyField(Genre)
+	publisher = models.ForeignKey(Publisher, on_delete=models.DO_NOTHING())
 
 	def __str__(self):
 		return self.title
